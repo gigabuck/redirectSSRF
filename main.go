c4ed48aebbbea3bf09a486b184fe8fc726b1a72d
Bucky Spires <gigabuck@users.noreply.github.com>
2020-09-10 21:40:25 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+    // http://127.0.0.1:8000/redirect?url=http://www.google.com
+	http.HandleFunc("/redirect", RedirectHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	log.Println("[*] Starting server on port " + port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func RedirectHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	url := query.Get("url")
+	http.Redirect(w, r, url, 301)
+}
